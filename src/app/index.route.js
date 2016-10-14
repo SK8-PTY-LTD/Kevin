c82eb7f01bd1b33334f1aa2709e4953319062dcb
kevinsu <szc769104@gmail.com>
2016-10-14 23:55:15 +1100

    html5 router

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -6,7 +6,7 @@
     .config(routerConfig);
 
   /** @ngInject */
-  function routerConfig($stateProvider, $urlRouterProvider) {
+  function routerConfig($stateProvider, $urlRouterProvider,$locationProvider) {
     $stateProvider
       .state('home', {
         url: '/',
@@ -16,6 +16,7 @@
       });
 
     $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(true);
   }
 
 })();
